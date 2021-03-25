1298135da1a71c8e51af8d29f94242361c681231
John Lake <john.f.lake.jr@gmail.com>
2021-03-25 14:08:34 -0400

    Slight refactor

--- src/main.c
+++ src/main.c
@@ -13,9 +13,15 @@
 #define GET_CONTENT 0x01
 #define GET_DISAMBIGUATION 0x02
 
-typedef struct userdata {
+typedef struct reqdata {
+
+  //Lets the underlying function know what API request to make
   int req_type;
-} userdata; 
+
+  //Response makes it clear that this is a disambiguation
+  bool is_disam;
+
+} reqdata; 
 
 /**
  * @brief Handle data from the web server.
@@ -28,7 +34,33 @@ static size_t handle_disam(char *ptr, size_t size, size_t nmemb, void * userdata
   return (size * nmemb);  
 }
 
-void get_disambiguation(char * wiki_page)
+/**
+ * @brief Handle data from the web server.
+ * @param ptr string of raw data received
+ */
+static size_t handle_resp(char *ptr, size_t size, size_t nmemb, void * userdata)
+{
+  char * content = parse_content_from_json(ptr); 
+  
+  if (NULL != content)
+  {
+
+    if (NULL != strstr(content, "may refer to"))
+    {
+      ((reqdata *) userdata)->is_disam = true;
+      printf("There are multiple pages.  Try one of these...\n"); 
+    }
+    else
+    {
+      ((reqdata *) userdata)->is_disam = false;
+      printf("%s\n",content); 
+    }
+  }
+
+  return (size * nmemb);  
+}
+
+void make_request(char * wiki_page, reqdata * request_data)
 {
   CURL *curl;
   CURLcode res;
@@ -47,20 +79,36 @@ void get_disambiguation(char * wiki_page)
   }
 
   char * base_wiki_url = "https://en.wikipedia.org/w/api.php";
+  char * com;
+  char * flags = "&formatversion=2&format=json"; 
+
+  if (GET_CONTENT == request_data->req_type)
+  {
+    com = "?action=query&prop=extracts&exsentences=10&exlimit=5&titles=";
+  }
+  else
+  {
+    com = "?action=query&prop=links&titles=";
+  }
 
   strncpy(request_url, base_wiki_url, strlen(base_wiki_url)); 
-
-  char * com = "?action=query&prop=links&titles=";
   strncat(request_url, com, strlen(com)); 
   strncat(request_url, wiki_page, strlen(wiki_page)); 
-
-  char * flags = "&formatversion=2&format=json"; 
   strncat(request_url, flags, strlen(flags));
 
   //printf("Request URL: %s\n",request_url); 
   curl_easy_setopt(curl, CURLOPT_URL, request_url);
   curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handle_disam);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) request_data);
+
+  if (GET_CONTENT == request_data->req_type)
+  {
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handle_resp);
+  }
+  else
+  {
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handle_disam);
+  }
 
   res = curl_easy_perform(curl);
 
@@ -70,103 +118,9 @@ void get_disambiguation(char * wiki_page)
   }
 
   curl_easy_cleanup(curl);
-
   free(request_url); 
 }
 
-/**
- * @brief Handle data from the web server.
- * @param ptr string of raw data received
- */
-static size_t handle_resp(char *ptr, size_t size, size_t nmemb, void *userdata)
-{
-  char * content = parse_content_from_json(ptr); 
-  
-  if (NULL != content)
-  {
-
-    if (NULL != strstr(content, "may refer to"))
-    {
-      *((int *) userdata) = 1;
-      printf("There are multiple pages.  Try one of these...\n"); 
-    }
-    else
-    {
-      *((int *) userdata) = 0;
-      printf("%s\n",content); 
-    }
-  }
-  
-#ifdef DEBUG
-  //printf("GOT %lu 'size'. \n\n\n", size); 
-  //printf("GOT %lu nmemb. \n\n\n", nmemb); 
-  //printf("GOT USER DATA: %s\n", userdata); 
-  //free(resp); 
-#endif
-
-  return (size * nmemb);  
-}
-
-
-void get_page_content(char * wiki_page)
-{
-  CURL *curl;
-  curl = curl_easy_init();
-
-  if (NULL == curl) 
-  {
-    return;
-  }
-
-  char * request_url = calloc(REQ_SIZE, sizeof(char)); 
-
-  if (NULL == request_url)
-  {
-    return;
-  }
-
-  char * base_wiki_url = "https://en.wikipedia.org/w/api.php";
-
-  strncpy(request_url, base_wiki_url, strlen(base_wiki_url)); 
-
-  char * com = "?action=query&prop=extracts&exsentences=10&exlimit=5&titles=";
-  strncat(request_url, com, strlen(com)); 
-
-  /*
-    Combine arguments to one page (e.g: combine apple iphone to apple_iphone
-  */
-  strncat(request_url, wiki_page, strlen(wiki_page)); 
-
-  char * flags = "&explaintext=1&formatversion=2&format=json"; 
-  strncat(request_url, flags, strlen(flags)); 
-
-#ifdef DEBUG 
-  //printf("Request URL: %s\n",request_url); 
-#endif
-
-  int disambiguation = 0; 
-    
-  curl_easy_setopt(curl, CURLOPT_URL, request_url);
-  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, handle_resp);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &disambiguation);
-
-  CURLcode res = curl_easy_perform(curl);
-
-  if (CURLE_OK != res)
-  {
-    fprintf(stderr, "err: %s\n", curl_easy_strerror(res));
-  }
-
-  curl_easy_cleanup(curl);
-
-  if (disambiguation)
-  {
-    get_disambiguation(wiki_page);
-  }
-
-  free(request_url); 
-}
 
 
 /*
@@ -230,7 +184,21 @@ int main(int argc, char ** argv)
   }
 
   char * wiki_page = combine_args_to_page(argc, argv);
-  get_page_content(wiki_page); 
+
+  //First, try making a request for the given page.
+  reqdata request;
+  request.req_type = GET_CONTENT; 
+  make_request(wiki_page, &request); 
+
+  //If that didn't work because of disambiguation, handle that: 
+
+  if (request.is_disam)
+  {
+    request.req_type = GET_DISAMBIGUATION;
+    make_request(wiki_page, &request); 
+  }
+
+
   
   return 0;
 }
